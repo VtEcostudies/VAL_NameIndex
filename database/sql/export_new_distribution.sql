e29cfb450e2ce57fc148b32b62fbd417a017933d
jloomisVCE <jloomis@vtecostudies.org>
2022-11-28 10:54:07 -0500

    Create export_new_distribution.sql

---
+++ database/sql/export_new_distribution.sql
@@ -0,0 +1,12 @@
+--NOTE: pg just needs permissions to write to the specified folder. chmod 777 \dir\subdir.
+--gbifId,establishmentMeans,locationID,locality,CountryCode
+--5389012,,ISO3166-2:US-VT,Vermont-US,US
+copy (select
+--"taxonId",
+"taxonId" AS "gbifID",
+'' AS "establishmentMeans",
+'ISO3166-2:US-VT' AS "locationID",
+'Vermont-US' AS "locality",
+'US' AS "CountryCode"
+from new_species)
+to 'C:\Users\jtloo\Documents\VCE\VAL_Data_Pipelines\VAL_NameIndex\repo\database\export\new_distribution.csv' delimiter ',' csv header;
